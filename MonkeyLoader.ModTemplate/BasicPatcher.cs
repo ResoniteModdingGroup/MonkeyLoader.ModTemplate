4b369d7533887dffe2ad3e0e916c07ad360d9979
Arne Kiesewetter <banane9@live.de>
2026-03-02 21:17:42 +0100

    Remove PrePatcher

--- MonkeyLoader.ModTemplate/BasicPatcher.cs
+++ MonkeyLoader.ModTemplate/BasicPatcher.cs
@@ -8,7 +8,7 @@ namespace MonkeyLoader.ModTemplate
 {
     [HarmonyPatchCategory(nameof(BasicPatcher))]
     [HarmonyPatch(typeof(ProtoFluxTool), nameof(ProtoFluxTool.OnAttach))]
-    internal sealed class BasicPatcher : ResoniteMonkey<BasicPatcher>
+    internal sealed class BasicPatcher : ConfiguredResoniteMonkey<BasicPatcher, MyConfig>
     {
         // The options for these should be provided by your game's game pack.
         protected override IEnumerable<IFeaturePatch> GetFeaturePatches()
